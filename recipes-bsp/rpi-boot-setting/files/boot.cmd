18af7c32bae69e633b67dc19bd33c2e88f898e40
Kazunori Kobayashi <kazunori.kobayashi@cybertrust.co.jp>
2023-06-30 15:27:35 +0000

    raspberrypi3bplus-64: Enable dtoverlay and dtparams

--- recipes-bsp/rpi-boot-setting/files/boot.cmd
+++ recipes-bsp/rpi-boot-setting/files/boot.cmd
@@ -1,4 +1,5 @@
 fatload mmc 0 ${kernel_addr_r} Image
-fatload mmc 0 ${fdt_addr_r} bcm2837-rpi-3-b-plus.dtb
+setenv fdt_addr_r 0x02600000
+fdt addr ${fdt_addr_r}
 setenv bootargs dwc_otg.lpm_enable=0 earlyprintk root=/dev/mmcblk0p2 rootfstype=ext4 rootwait
 booti ${kernel_addr_r} - ${fdt_addr_r}
