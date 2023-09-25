8df1c5607ee3b6d208371c3b89d69933c5fa3101
Hotaka Miyazaki <hotaka.miyazaki@miraclelinux.com>
2023-09-25 18:10:06 +0900

    machine: rapsberrypi4: Add initial support for Raspberry Pi 4 Model B
    Add recipes to build an image for raspberrypi4.
    
    Note that this is experimental, and not intended for use in production yet.

--- recipes-bsp/rpi-boot-setting/files/boot.cmd
+++ recipes-bsp/rpi-boot-setting/files/boot.cmd
@@ -1,4 +1,9 @@
-fatload mmc 0 ${kernel_addr_r} Image
-fatload mmc 0 ${fdt_addr_r} bcm2837-rpi-3-b-plus.dtb
-setenv bootargs dwc_otg.lpm_enable=0 earlyprintk root=/dev/mmcblk0p2 rootfstype=ext4 rootwait
-booti ${kernel_addr_r} - ${fdt_addr_r}
+setenv setbootargs 'setenv bootargs dwc_otg.lpm_enable=0 earlyprintk root=PARTUUID=${uuid} rootfstype=ext4 rootwait'
+setenv setuuid 'part uuid mmc ${devnum}:2 uuid'
+setenv bootlinux 'run setuuid && run setbootargs && fatload mmc ${devnum} ${kernel_addr_r} Image && fatload mmc ${devnum} ${fdt_addr_r} ${fdt_name} && booti ${kernel_addr_r} - ${fdt_addr_r}'
+
+setenv boot_rpi3 'setenv fdt_name bcm2837-rpi-3-b-plus.dtb && setenv devnum 0 && run bootlinux'
+setenv boot_rpi4 'setenv fdt_name bcm2711-rpi-4-b.dtb && setenv devnum 1 && run bootlinux'
+
+run boot_rpi3
+run boot_rpi4
--- recipes-kernel/linux/linux-cip_git.bb
+++ recipes-kernel/linux/linux-cip_git.bb
@@ -22,6 +22,7 @@ SRC_URI:append:qemu-arm64 = " file://qemu-arm64_defconfig"
 SRC_URI:append:qemu-amd64 = " file://qemu-amd64_defconfig"
 SRC_URI:append:generic-x86-64 = " file://generic-x86-64_defconfig"
 SRC_URI:append:raspberrypi3bplus-64 = " file://raspberrypi3-64_defconfig"
+SRC_URI:append:raspberrypi4-64 = " file://raspberrypi4-64_defconfig"
 
 SRC_URI[sha256sum] = "1caa1b8e24bcfdd55c3cffd8f147f3d33282312989d85c82fc1bc39b808f3d6b"
 SRCREV = "58f7d1bf36cb697e7cab922b71d04f7d925e3b1f"
