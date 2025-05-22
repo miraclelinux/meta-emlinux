ff37d9f21a6d307ee6dd7dbc488a3b07c793de33
Takahiro Terada <takahiro.terada@miraclelinux.com>
2025-05-22 14:47:12 +0900

    Add docker files
    Add Dokerfile, docker-compose.yml, and run.sh to support building an image in
    docker container.
    
    Signed-off-by: Takahiro Terada <takahiro.terada@miraclelinux.com>

---
+++ docker/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+script_dir=$(dirname $0)
+docker_compose_cmd="docker compose"
+docker compose version > /dev/null 2>&1
+if [ $? != 0 ]; then
+    docker-compose version > /dev/null 2>&1
+    if [ $? != 0 ]; then
+	echo "[*] docker compose or docker-compose command is not found."
+	echo "    Please install newer version of docker engine (or docker-compose)."
+	exit 1
+    fi
+    docker_compose_cmd="docker-compose"
+fi
+
+mode="run"
+if [ $# = 1 ]; then
+  mode="${1}"
+fi
+
+host_user_id=$(id -u)
+host_user_name=$(id -un)
+export host_user_id="${host_user_id}"
+export host_user_name="${host_user_name}"
+
+cd ${script_dir}
+if [ "${mode}" = "build" ]; then
+  ${docker_compose_cmd} build --no-cache emlinux2-build
+elif [ "${mode}" = "run" ]; then
+  ${docker_compose_cmd} run --rm emlinux2-build
+elif [ "${mode}" = "clean" ]; then
+  docker rmi -f "emlinux2-build-${host_user_name}"
+fi
