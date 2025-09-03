946b8c5d731cd694da09153125d65ebbd573e963
Daisuke Yamane <daisuke.yamane@miraclelinux.com>
2025-09-03 20:11:28 +0900

    sdk: Run patchelf twice on the listed binaries
    Some binaries have been confirmed to cause a segmentation fault when patchelf is run once.
    To avoid this, run patchelf twice. However, running patchelf multiple times on all binaries
    may also cause a segmentation fault(due to other issue), so only run it twice on the binaries
    listed.

--- recipes-devtools/sdk-files/files/relocate-sdk.sh
+++ recipes-devtools/sdk-files/files/relocate-sdk.sh
@@ -9,16 +9,36 @@ sdkroot=$(realpath $(dirname $0))
 arch=$(uname -m)
 
 new_sdkroot=$sdkroot
+repatch_list=""
 
-case "$1" in
---help|-h)
-	echo "Usage: $0 [--restore-chroot|-r]"
-	exit 0
-	;;
---restore-chroot|-r)
-	new_sdkroot=/
-	;;
-esac
+opt_short="hrl:"
+opt_long="help,resore-chroot,repatch-list:"
+OPTS=$(getopt -o "$opt_short" -l "$opt_long" -- "$@")
+if [ $? -ne 0 ] ; then
+	echo "Wrong input parameter!"; 1>&2
+	exit 1;
+fi
+eval set -- "$OPTS"
+
+while true; do
+	case "$1" in
+	--help|-h)
+		echo "Usage: $0 [--restore-chroot|-r] [--repatch-list|-l <list>]"
+		exit 0
+		;;
+	--restore-chroot|-r)
+		new_sdkroot=/
+		shift
+		;;
+	--repatch-list|-l)
+		repatch_list=${2}
+		shift 2
+		;;
+	--)
+		shift; break
+		;;
+	esac
+done
 
 if [ -z $(which patchelf 2>/dev/null) ]; then
 	echo "Please install 'patchelf' package first."
@@ -45,6 +65,29 @@ for library in $(find ${sdkroot}/usr/lib -type f,l -name "lib*.so*" -exec file -
 		patchelf --set-rpath ${new_sdkroot}/usr/lib:${new_sdkroot}/usr/lib/${arch}-linux-gnu \
 		--force-rpath \
 		$library 2>/dev/null
+	fi
+done
+
+## HACK: Listed binaries require applying patchelf twice to avoid segfaults.
+for binary in $(cat $repatch_list ${new_sdkroot}/repatch.list); do
+	binary=${sdkroot}/${binary}
+	if file -L ${binary} |grep "executable" 2>&1 >/dev/null; then
+		interpreter=$(patchelf --print-interpreter ${binary} 2>/dev/null)
+		oldpath=${interpreter%/lib*/ld-linux*}
+		interpreter=${interpreter#${oldpath}}
+		if [ -n "${interpreter}" ]; then
+			patchelf --set-interpreter ${new_sdkroot}${interpreter} \
+				--set-rpath ${new_sdkroot}/usr/lib:${new_sdkroot}/usr/lib/${arch}-linux-gnu \
+				--force-rpath \
+				$binary 2>/dev/null
+		fi
+	else
+		rpath=$(patchelf --print-rpath ${binary})
+		if [ -n "${binary}" ]; then
+			patchelf --set-rpath ${new_sdkroot}/usr/lib:${new_sdkroot}/usr/lib/${arch}-linux-gnu \
+			--force-rpath \
+			$library 2>/dev/null
+		fi
 	fi
 done
 
